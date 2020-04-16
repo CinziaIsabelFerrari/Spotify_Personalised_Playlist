ec0e62291bf5a69232a9825d44bb91acb99f67ad
CinziaIsabelFerrari <CinziaIsabelFerrari@users.noreply.github.com>
2020-04-16 18:44:40 +0100

    playlist with 3 genres with input

--- spotify_playlist.py
+++ spotify_playlist.py
@@ -13,28 +13,80 @@ token = util.prompt_for_user_token(username, scope)
 endpoint_url = "https://api.spotify.com/v1/recommendations?"
 
 #my filters:
-limit = 10
-uris = []
+limit = 4
+uris1 = []
+
 market = "GB" #can choose country code asking via input
-seed_genres = "indie" #can choose genre via input // look for a list of genres
+#country = "GB"
+#locale = "es_ES" #language of the tracks
+#seed_genres = "jazz" #can choose genre via input // look for a list of genres
 target_danceability = 0.9 # goes from 0 to 1
 #min_popularity = which are the values?
 target_popularity = 50 # goes from 0 to 100
 # can you add mood on top of genres, and the language instead of country?
 
-query = '{}limit={}&market={}&seed_genres={}&target_danceability={}&target_popularity={}'.format(endpoint_url, limit, market, seed_genres, target_danceability, target_popularity)
+genre1 = input('Choose a genre between indie, jazz, classical, dance, groove: ')
 
-response = requests.get(query,
+query1 = '{}limit={}&seed_genres={}&target_danceability={}&target_popularity={}&market={}'.format(
+    endpoint_url,
+    limit,
+    genre1,
+    target_danceability,
+    target_popularity,
+    market)
+
+response1 = requests.get(query1,
                         headers = {'Content-Type':'application/json',
                                    'Authorization':'Bearer {}'.format(token)})
 
-json_response = response.json()
-print(json_response)
+json_response = response1.json()
+
+genre2 = input('Choose a genre between rock, disco, electronic, funk, rockabilly: ')
+
+query2 = '{}limit={}&seed_genres={}&target_danceability={}&target_popularity={}&locale={}&country={}'.format(
+    endpoint_url,
+    limit,
+    genre2,
+    target_danceability,
+    target_popularity,
+    locale,
+    country)
+
+response2 = requests.get(query2,
+                        headers = {'Content-Type':'application/json',
+                                   'Authorization':'Bearer {}'.format(token)})
+
+json_response2 = response2.json()
+
+genre3 = input('Choose a genre between blues, funk, groove, hip-hop, rock-n-roll ')
+
+query3 = '{}limit={}&seed_genres={}&target_danceability={}&target_popularity={}&locale={}&country={}'.format(
+    endpoint_url,
+    limit,
+    genre3,
+    target_danceability,
+    target_popularity,
+    locale,
+    country)
+
+response3 = requests.get(query3,
+                        headers = {'Content-Type':'application/json',
+                                   'Authorization':'Bearer {}'.format(token)})
+
+json_response3 = response3.json()
+print(json_response3)
 
 for i,j in enumerate (json_response['tracks']):
-    uris.append(j['uri'])
+    uris1.append(j['uri'])
     print('{})\"{}\" by \"{}\"'.format(i+1, j['name'],j['artists'][0]['name']))
 
+for i,j in enumerate (json_response2['tracks']):
+    uris1.append(j['uri'])
+    print('{})\"{}\" by \"{}\"'.format(i+5, j['name'],j['artists'][0]['name']))
+
+for i,j in enumerate (json_response3['tracks']):
+    uris1.append(j['uri'])
+    print('{})\"{}\" by \"{}\"'.format(i+9, j['name'],j['artists'][0]['name']))
 
 
 #create a playlist
@@ -58,9 +110,8 @@ playlist_id = response.json()['id']
 
 endpoint_url = "https://api.spotify.com/v1/playlists/{}/tracks".format(playlist_id)
 
-request_body = json.dumps({
-          "uris" : uris
-        })
+request_body = json.dumps({"uris" : uris1})
+
 response = requests.post(url = endpoint_url, data = request_body, headers={"Content-Type":"application/json",
                         'Authorization':'Bearer {}'.format(token)})
 
